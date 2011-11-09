cb3447e5fda09b08e8990ee4d85502754447e311
xdu <duxinge@gmail.com>
2011-11-09 14:56:07 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ralavada::Application.config.secret_token = 'f62730303e0a2a71390635f6be8b572927ab21a000e4cbbafe7cc5565388479a1b4ef1114858b0428e67f61efdac07941ccd112aef44ae55b5c40adde58dc454'
